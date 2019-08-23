e80dad8e03426b54bf686b202f3ee22e66825303
Lorenzo Brivio <lorenzo.brivio9908@gmail.com>
2019-08-23 14:03:54 +0200

    First commit. Basic feature

---
+++ random_quiz.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+
+# LORENZO BRIVIO - random italian drive license quiz opener
+
+import sys
+import webbrowser as wb
+import random
+
+sito = 'https://www.patentati.it/quiz-patente-b/esame-%d.html'
+
+if not len(sys.argv) == 2 or not sys.argv[1] == '-y':
+    input('Click enter to open a random quiz: ')
+
+n = random.randint(1, 657)
+print('Scheda ministeriale n.%d' % n)
+wb.open_new_tab(sito % n)
